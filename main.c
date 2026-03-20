400babd0229daddf5f699005286e228453ea7f48
mateusofneves <prmateus.ofn@gmail.com>
2026-03-20 11:39:29 -0300

    comentario 3 mateus - comentario 3

--- main.c
+++ main.c
@@ -4,9 +4,12 @@ int main(){
 
     printf("Paulo Henrique");
 
+    //comentarios mateus commit
     printf("Mateus Neves");
     printf("Hello world!")
     printf("Professor Erick")
+    printf("Comentário 3 Mateus")
+
 
     return 0;
 
